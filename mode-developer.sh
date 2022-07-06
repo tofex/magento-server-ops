c47a9cae5634d140b112b5a9445653ee30657c9b
Andreas Knollmann <aknollmann@tofex.de>
2022-07-06 14:56:26 +0200

    added mode setting

---
+++ mode-developer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+scriptPath="${BASH_SOURCE[0]}"
+
+if [[ -L "${scriptPath}" ]]; then
+  scriptPath=$(realpath "${scriptPath}")
+fi
+
+currentPath="$( cd "$( dirname "${scriptPath}" )" && pwd )"
+
+"${currentPath}/../core/script/run.sh" "install,webServer" "${currentPath}/mode-developer/web-server.sh"
